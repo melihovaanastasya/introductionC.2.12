36ba2be7f4607b051f5a3c76f56676dc3b881484
Anastasiya <melihovaanastasya@yandex.ru>
2022-12-02 20:26:27 +0300

    70

---
+++ 70/Program.cs
@@ -0,0 +1,12 @@
+﻿// 70. Показать натуральные числа от 1 до N, N задано
+
+void Loop (int N, int i)
+{
+    if (i<=N)
+    {
+        System.Console.WriteLine(i);
+        Loop(N,i+1);
+    }
+}
+
+Loop(10,1);
