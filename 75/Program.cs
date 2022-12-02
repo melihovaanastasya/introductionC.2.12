95d7f3acc2172bd03302a464d760681ec860baa5
Anastasiya <melihovaanastasya@yandex.ru>
2022-12-02 22:48:14 +0300

    75,77

---
+++ 75/Program.cs
@@ -0,0 +1,10 @@
+﻿// 75. Написать программу вычисления функции Аккермана
+
+int Akker (int m, int n)
+{
+    if (m==0) return (n+1);
+    if (n==0 && m>0) return Akker(m-1,1);
+    else return Akker(m-1,Akker(m,n-1));
+}
+
+System.Console.WriteLine(Akker(2,2));
--- Fibbon/Program.cs
+++ Fibbon/Program.cs
@@ -1,2 +1,10 @@
 ﻿// числа Фиббоначи
 
+int Fib (int N)
+{
+    if (N==1) return (N-1); 
+    if (N==2) return (N-1); 
+    else return (Fib(N-1)+Fib(N-2));
+}
+
+System.Console.WriteLine(Fib(10));
